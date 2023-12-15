e0f2f085c481ce5be17e5d0e11543906fb631a7e
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-15 11:19:21 +0700

    feat: add operator perbandingan dart

---
+++ belajar_dart_1/bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('aisyah' == 'aisyah');
+  print('aisyah' != 'aisyah');
+
+}
