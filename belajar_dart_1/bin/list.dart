0d8286623d97379f2a394cf0fc709c620e912cd0
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-16 12:11:51 +0700

    feat: add list in dart

---
+++ belajar_dart_1/bin/list.dart
@@ -0,0 +1,27 @@
+void main() {
+
+  List<int> listInt = [];
+  var listString = <String> [];
+
+  print(listInt);
+  print(listString);
+
+  var names = <String> [];
+  names.add('Aisyah');
+  names.add('Nur');
+  names.add('Fadilah');
+
+  print(names);
+  print(names.length);
+
+  print(names[0]);
+
+  names[0] = 'Budi';
+  print(names[0]);
+
+  names.removeAt(0);
+  print(names);
+  
+  print(names[0]);
+
+}
