c6ce88c5940dc84e7013e7e53ac6239917f7164e
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-16 11:08:30 +0700

    feat: add increment decrement in dart

--- belajar_dart_1/bin/operator_penugasan.dart
+++ belajar_dart_1/bin/operator_penugasan.dart
@@ -9,4 +9,15 @@ void main() {
   print(b /= 10);
   print(a ~/= 10);
   print(a %= 10);
+
+  // increment, decrement
+  var i = 0;
+  // i = i + i
+
+  var j = i++; // j = i, i++
+  var k = ++i; // k = (++i)
+
+  print(i);
+  print(j);
+  print(k);
 }
