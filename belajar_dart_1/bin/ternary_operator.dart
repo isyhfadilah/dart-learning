280d7f5ad8768cded0ee8f6692980c082ec46351
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-16 13:54:17 +0700

    feat: add ternary operator in dart

---
+++ belajar_dart_1/bin/ternary_operator.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  var nilai = 75;
+  var ucapan = nilai >= 75 ? 'Selamat Anda Lulus' : 'Silahkan Coba Lagi';
+
+  // String ucapan;
+  // if(nilai >= 75) {
+  //   ucapan = 'Selamat Anda Lulus';
+  // } else {
+  //   ucapan = 'Silahkan Coba Lagi';
+  // }
+
+  print(ucapan);
+  
+}
