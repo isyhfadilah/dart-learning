1d00a422a74a30472c5b8c18f6f8fa8ac236959f
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-18 16:19:17 +0700

    feat: add scope in dart

---
+++ belajar_dart_1/bin/scope.dart
@@ -0,0 +1,15 @@
+void main() {
+  var name = 'Aisyah';
+
+  void sayHello() {
+    var hello = 'Hello $name';
+    print(hello);
+  }
+
+  sayHello();
+  // print(hello); -- error
+}
+
+void contoh(){
+  // sayHello(); -- error
+}
