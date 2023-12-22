b81c2dcd13317d3bd4a28f2bd6862c7ab6c62dbc
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-22 21:10:40 +0700

    feat: add object in opp dart

--- belajar_oop_dart/bin/class.dart
+++ belajar_oop_dart/bin/class.dart
@@ -1,3 +1,14 @@
 class Person {
-  
+
+}
+
+void main () {
+
+  // bisa gunakan keyword new, tpi tidak direkomendasikan di dart (v baru)
+  var person1 = Person();
+  print(person1);
+
+  Person person2 = Person();
+  print(person2);
+
 }
