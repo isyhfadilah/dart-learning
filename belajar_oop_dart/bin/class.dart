d39ede588c99f66b201a0199cb90c37a453c1c16
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-22 21:23:19 +0700

    fix: manipulasi field in oop dart

--- belajar_oop_dart/bin/class.dart
+++ belajar_oop_dart/bin/class.dart
@@ -10,6 +10,10 @@ void main () {
 
   // bisa gunakan keyword new, tpi tidak direkomendasikan di dart (v baru)
   var person1 = Person();
+  person1.name = "Aisyah Nur Fadilah";
+  person1.address = "Sumedang";
+  // person1.country = "Singapore"; field final tidak dapat dirubah
+
   print(person1.name);
   print(person1.address);
   print(person1.country);
