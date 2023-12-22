f479bf4adcf3b4923b2131e69ecc8cc636d5d755
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-22 21:17:42 +0700

    feat: add field in oop dart

--- belajar_oop_dart/bin/class.dart
+++ belajar_oop_dart/bin/class.dart
@@ -1,12 +1,18 @@
 class Person {
 
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
 }
 
 void main () {
 
   // bisa gunakan keyword new, tpi tidak direkomendasikan di dart (v baru)
   var person1 = Person();
-  print(person1);
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 
   Person person2 = Person();
   print(person2);
