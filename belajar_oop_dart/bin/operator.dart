69c7a8877f2b422f4915a85774b3cae15b687037
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-25 22:57:06 +0700

    feat: akses operator in oop dart

--- belajar_oop_dart/bin/operator.dart
+++ belajar_oop_dart/bin/operator.dart
@@ -6,5 +6,18 @@ class Orange {
     result.quantity = quantity + other.quantity;
     return result;
   }
-  
+
+}
+
+void main() {
+
+  var orange1 = Orange();
+  orange1.quantity = 10;
+
+  var orange2 = Orange();
+  orange2.quantity = 10;
+
+  var orange3 = orange1 + orange2;
+  print(orange3.quantity);
+
 }
