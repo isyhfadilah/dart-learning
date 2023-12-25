7f2c036a3ec10267039a8b55f2c0e5c4a92ec8c1
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-25 22:29:41 +0700

    feat: add method expression body in oop dart

---
+++ belajar_oop_dart/bin/method_expression_body.dart
@@ -0,0 +1,14 @@
+class Computer {
+
+  // void startup() {
+  //   print('computer is starting');
+  // }
+  void startup() => print('computer is starting');
+  void shutdown() => print('computer is shutting down');
+
+  // String getOperatingSystem() {
+  //   return 'window';
+  // }
+  String getOperatingSystem() => "window";
+  
+}
