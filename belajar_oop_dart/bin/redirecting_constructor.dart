b361ab6e41434a3627bb71ef9aed12e6b5dca765
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-26 16:52:04 +0700

    feat: add redirecting constructor in oop dart

--- belajar_oop_dart/bin/redirecting_constructor.dart
+++ belajar_oop_dart/bin/redirecting_constructor.dart
@@ -10,6 +10,10 @@ class Person {
 
   Person.withAddress(String address) : this("No name", address);
 
+  Person.fromJakarta() : this.withAddress("Jakarta");
+
+  Person.noName() : this.withName("No name");
+
 }
 
 void main() {
@@ -25,5 +29,13 @@ void main() {
   var person3 = Person.withAddress('Bandung');
   print(person3.name);
   print(person3.address);
+
+  var person4 = Person.fromJakarta();
+  print(person4.name);
+  print(person4.address);
+
+  var person5 = Person.noName();
+  print(person5.name);
+  print(person5.address);
 
 }
