df78660a3d6b254a7a1ae3651ecc1d22d2bfda83
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-26 16:02:46 +0700

    feat: add variable shadowing in oop dart

---
+++ belajar_oop_dart/bin/varible_shadowing.dart
@@ -0,0 +1,20 @@
+class Person {
+
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+
+  Person(String name, String address) {
+    name = name;
+    address = address;
+  }
+
+}
+
+void main() {
+
+  var person = Person('Aisyah', 'Sumedang');
+  print(person.name);
+  print(person.address);
+
+}
