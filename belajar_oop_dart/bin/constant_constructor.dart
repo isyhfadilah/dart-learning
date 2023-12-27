4a7f249ba91352c0618369d8ed7cc1a15d9cb27b
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-27 16:06:58 +0700

    feat: add constant constructor in oop dart

---
+++ belajar_oop_dart/bin/constant_constructor.dart
@@ -0,0 +1,20 @@
+class ImmutablePoint {
+
+  final int x;
+  final int y;
+
+  const ImmutablePoint(this.x, this.y);
+
+}
+
+void main() {
+
+  var point1 = const ImmutablePoint(10, 10);
+  var point2 = const ImmutablePoint(10, 10);
+
+  print(point1 == point2);
+
+  print(point1.x);
+  print(point2.x);
+
+}
