85421c52ff6f3d8b50eb95d7e88b8b6a8cf6f657
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-07 14:31:30 +0700

    fix: expression body on getter setter in oop dart

--- belajar_oop_dart/bin/data/rectangle.dart
+++ belajar_oop_dart/bin/data/rectangle.dart
@@ -3,20 +3,26 @@ class Rectangle {
   int _width = 0;
   int _length = 0;
 
-  int get width {
-    return _width;
-  }
+  // int get width {
+  //   return _width;
+  // }
 
-  set width(int value) {
-    _width = value;
-  }
+  // set width(int value) {
+  //   _width = value;
+  // }
 
-  int get length {
-    return _length;
-  }
+  // int get length {
+  //   return _length;
+  // }
 
-  set length(int value) {
-    _length = value;
-  }
+  // set length(int value) {
+  //   _length = value;
+  // }
 
+  int get width => _width;
+  set width(int value) => _width = value;
+
+  int get length => _length;
+  set length(int value) => _length = value;
+  
 }
