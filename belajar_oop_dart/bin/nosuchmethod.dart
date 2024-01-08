0965b872a7f510381f057da1fa20ec67e4ad319a
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-08 23:41:57 +0700

    feat: add no such method in oop dart

---
+++ belajar_oop_dart/bin/nosuchmethod.dart
@@ -0,0 +1,11 @@
+import 'data/repository.dart';
+
+void main() {
+
+  dynamic repository = Repository('products');
+  
+  repository.id('1');
+  repository.name('Laptop');
+  repository.quantity(1000);
+  
+}
