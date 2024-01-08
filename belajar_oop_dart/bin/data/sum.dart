fcf725b06c7c8951e4bafea69a639a420e7157ae
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-09 00:16:17 +0700

    feat: add typedef for alias in oop dart

--- belajar_oop_dart/bin/data/sum.dart
+++ belajar_oop_dart/bin/data/sum.dart
@@ -5,15 +5,16 @@ class Sum {
 
   Sum(this.first, this.second);
 
-  int call() {
-    return first + second;
-  }
+  int call() => first + second;
 
 }
+
+typedef Total = Sum;
+typedef Jumlah = Sum;
 
 void main() {
 
   var sum = Sum(10, 20);
   print(sum());
-  
+
 }
