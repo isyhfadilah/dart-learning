c78a5dfcc776c13fc1aee606c3e8048fdced6454
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-09 00:08:54 +0700

    feat: add callable class in oop dart

---
+++ belajar_oop_dart/bin/data/sum.dart
@@ -0,0 +1,19 @@
+class Sum {
+
+  int first;
+  int second;
+
+  Sum(this.first, this.second);
+
+  int call() {
+    return first + second;
+  }
+
+}
+
+void main() {
+
+  var sum = Sum(10, 20);
+  print(sum());
+  
+}
